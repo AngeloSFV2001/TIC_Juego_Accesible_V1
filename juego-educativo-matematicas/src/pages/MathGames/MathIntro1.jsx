7773116cb6b8e0ba0df6c842c742a81f79226f2c
AngeloSFV2001 <angelo.fabara@epn.edu.ec>
2025-01-17 15:57:50 -0500

    video

--- juego-educativo-matematicas/src/pages/MathGames/MathIntro1.jsx
+++ juego-educativo-matematicas/src/pages/MathGames/MathIntro1.jsx
@@ -15,12 +15,14 @@ const MathIntro = () => {
       {/* Video responsivo */}
       <div className="mb-4" style={{ width: '100%', maxWidth: '800px' }}>
         <div className="ratio ratio-16x9">
-        <iframe width="560" 
-        height="315" 
-        src="https://www.youtube.com/embed/Y8q_74xXN20?si=0rpGU8STKMxjRYpo" 
-        title="YouTube video player" 
-        frameborder="0" 
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe 
+        width="560" 
+        height="315"
+         src="https://www.youtube.com/embed/0n0Tj2oRFEw?si=tLxPc6vpjpVfzU1b" 
+         title="YouTube video player"
+         frameborder="0" 
+         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+         referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
       </div>
 
